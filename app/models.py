0fe2a576d05d74e11fcb53eec01cbaafd2c1b810
kfqyz <sh48@qq.com>
2018-11-09 11:33:13 +0800

    2018.11.8

--- app/models.py
+++ app/models.py
@@ -19,10 +19,31 @@ class Role(UserMixin, db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean, default=False, unique=True)
+    permissions = db.Column(db.Integer)
     users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
         return '<Role {}>'.format(self.name)
+
+    def __init__(self, **kwargs):
+        super(Role, self).__init__(**kwargs)
+        if self.permissions is None:
+            self.permissions = 0
+
+    def has_permission(self, perm):
+        return self.permissions & perm == perm
+
+    def add_permission(self, perm):
+        if not self.has_permission(perm):
+            self.permissions += perm
+
+    def remove_permission(self, perm):
+        if self.has_permission(perm):
+            self.permissions -= perm
+
+    def reset_permission(self):
+        self.permissions = 0
 
 
 class User(UserMixin, db.Model):
@@ -51,17 +72,18 @@ class User(UserMixin, db.Model):
         s = Serializer(current_app.config['SECRET_KEY'], expiration)
         return s.dumps({'confirm': self.id}).decode('utf-8')
 
-    def confirm(self,token):
-        s =Serializer(current_app.config['SECRET_KEY'])
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
         try:
             data = s.loads(token.encode('utf-8'))
         except:
             return False
-        if data.get('confirm')!=self.id:
+        if data.get('confirm') != self.id:
             return False
-        self.confirmed=True
+        self.confirmed = True
         db.session.add(self)
         return True
+
 
 @login_manager.user_loader
 def load_user(user_id):
