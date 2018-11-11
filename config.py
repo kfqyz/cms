be7820ef19770198254105a77057ce2b59607262
kfqyz <sh48@qq.com>
2018-11-11 11:23:33 +0800

    2018.11.8

--- config.py
+++ config.py
@@ -3,15 +3,16 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = 'hard to guess string'
-    MAIL_SERVER = 'smtp.163.com'
-    MAIL_PORT = 994
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = 'kfqyz@163.com'
-    MAIL_PASSWORD = ''
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
+                   ['true', 'on', '1']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     SCHOA_MAIL_SUBJECT_PREFIX = '[SCHOA]'
-    SCHOA_MAIL_SENDER = 'SCHOA Admin <kfqyz@163.com>'
-    SCHOA_ADMIN = 'kfqyz@163.com'
+    SCHOA_MAIL_SENDER = 'SCHOA Admin <SCHOA@example.com>'
+    SCHOA_ADMIN = os.environ.get('SCHOA_ADMIN')
     SSL_REDIRECT = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_RECORD_QUERIES = True
@@ -19,6 +20,22 @@ class Config:
     SCHOA_FOLLOWERS_PER_PAGE = 50
     SCHOA_COMMENTS_PER_PAGE = 30
     SCHOA_SLOW_DB_QUERY_TIME = 0.5
+    # SECRET_KEY = 'hard to guess string'
+    # MAIL_SERVER = 'smtp.163.com'
+    # MAIL_PORT = 994
+    # MAIL_USE_TLS = True
+    # MAIL_USERNAME = 'kfqyz@163.com'
+    # MAIL_PASSWORD = ''
+    # SCHOA_MAIL_SUBJECT_PREFIX = '[SCHOA]'
+    # SCHOA_MAIL_SENDER = 'SCHOA Admin <kfqyz@163.com>'
+    # SCHOA_ADMIN = 'kfqyz@163.com'
+    # SSL_REDIRECT = False
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # SQLALCHEMY_RECORD_QUERIES = True
+    # SCHOA_POSTS_PER_PAGE = 20
+    # SCHOA_FOLLOWERS_PER_PAGE = 50
+    # SCHOA_COMMENTS_PER_PAGE = 30
+    # SCHOA_SLOW_DB_QUERY_TIME = 0.5
 
     @staticmethod
     def init_app(app):
